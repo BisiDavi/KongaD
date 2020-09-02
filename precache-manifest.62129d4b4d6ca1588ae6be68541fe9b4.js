20bbb3649fa31077e149b920e6d7b40ffda1ffce
BisiDavi <oludavidconnect@gmail.com>
2020-09-02 02:29:23 +0100

    Updates

---
+++ precache-manifest.62129d4b4d6ca1588ae6be68541fe9b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c8c6103d6bc546aa0ec929ce72683a63",
+    "url": "/bisidavi.github.io/KongaD/index.html"
+  },
+  {
+    "revision": "edf0d0d4de04779a36fc",
+    "url": "/bisidavi.github.io/KongaD/static/css/2.fb346933.chunk.css"
+  },
+  {
+    "revision": "7f77277c964988bb8a5d",
+    "url": "/bisidavi.github.io/KongaD/static/css/main.6ba9bd7a.chunk.css"
+  },
+  {
+    "revision": "edf0d0d4de04779a36fc",
+    "url": "/bisidavi.github.io/KongaD/static/js/2.6f7eaa0b.chunk.js"
+  },
+  {
+    "revision": "872d4ad223c39fec1068d862c0b329ff",
+    "url": "/bisidavi.github.io/KongaD/static/js/2.6f7eaa0b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f77277c964988bb8a5d",
+    "url": "/bisidavi.github.io/KongaD/static/js/main.c337dc7c.chunk.js"
+  },
+  {
+    "revision": "b51e1c89ef1617c541b0",
+    "url": "/bisidavi.github.io/KongaD/static/js/runtime-main.d4c4bfa6.js"
+  }
+]);
